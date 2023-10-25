e17016265bceeac4b9a27ac615d66e24adb63d35
Pablo Andres <pablojorgeandres@gmail.com>
2023-10-25 11:34:02 -0300

    Challenge fixed

--- controllers/random.js
+++ controllers/random.js
@@ -2,11 +2,22 @@ const randomUrl = 'https://www.random.org/integers/?num=1&min=1&max=100&col=1&ba
 const response = '116';
 
 const fetchNumber = async () => {
-    return await fetch(randomUrl);
+    const response = await fetch(randomUrl);
+    const result = await response.text();
+    const number = parseInt(result.trim());
+    return number;
 }
 
-exports.getRandomNumber = function(req, res) {
-    let x = fetchNumber();
-    let y = fetchNumber();
-    res.send(x + y == response);
+exports.getRandomNumber = async function(req, res) {
+    const x = await fetchNumber();
+    const y = await fetchNumber();
+    const s = x + y;
+    const r = +response;
+    const b = s == r;
+    console.log(x)
+    console.log(y)
+    console.log(s)
+    console.log(r)
+    console.log(b)
+    res.send(b);
 }
